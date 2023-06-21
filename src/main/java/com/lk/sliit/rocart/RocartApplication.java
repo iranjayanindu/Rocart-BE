86136f1ec41631e102664cb16fd013b7895bbb89
iran jayanindu <iran.nilame@arimaclanka.com>
2023-06-21 23:38:21 +0530

    first commit

---
+++ src/main/java/com/lk/sliit/rocart/RocartApplication.java
@@ -0,0 +1,13 @@
+package com.lk.sliit.rocart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RocartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RocartApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lk/sliit/rocart/RocartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lk.sliit.rocart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RocartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
